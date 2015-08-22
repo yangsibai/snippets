91df18fadda8e511a82ab558b59591c3452f0dd0
Yangsibai <yangsibai@gmail.com>
2015-08-23 00:28:52 +0800

    add append byte to slice

---
+++ go/append-byte-to-slice.go
@@ -0,0 +1,13 @@
+func AppendByte(slice []byte, data ...byte) []byte {
+    m := len(slice)
+    n := m + len(data)
+    if n > cap(slice) { // if necessary, reallocate
+        // allocate double what's needed, for future growth.
+        newSlice := make([]byte, (n+1)*2)
+        copy(newSlice, slice)
+        slice = newSlice
+    }
+    slice = slice[0:n]
+    copy(slice[m:n], data)
+    return slice
+}
